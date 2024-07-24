35d30853fdcbafbc6d48d2b7b228996c2cd89524
MarcosVinicius <mar.vini.20144@gmail.com>
2024-07-23 21:31:58 -0300

    aprendi como somar variáveis em python

--- paldeck.py
+++ paldeck.py
@@ -1,5 +1,11 @@
 nome =input("Digite Seu Nome:")
 print(f"pawldex de {nome}")
 
-quantidade_pawl = input("Quantos pawls você tem?")
+quantidade_pawl = int(input("Quantos pawls você tem?"))
 print(f"{nome} possui {quantidade_pawl} pawls")
+
+inserir = int(input("Quantos novos voçê capturou?"))
+
+nova_quntidade = quantidade_pawl + inserir
+
+print(f"{nome} capturou {inserir} pawls novos e agora tem {nova_quntidade}")
